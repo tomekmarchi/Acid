abb606fb0720bdabb4e0b74f2afd210b83c78dfa
Tomek Marchi <tomekmarchi@gmail.com>
2017-06-28 01:26:04 -0400

    Too vague now not vague

--- source/utility/each.js
+++ source/utility/each.js
@@ -1,24 +1,24 @@
 import acid from '../namespace/index';
 import { assign } from '../internal/object';
-import { hasValue, isPlainObject, isFunction, isArray } from '../internal/is';
-import { mapObject, eachObject, filterObject } from '../object/each';
-import { eachArray, mapArray, filterArray } from '../array/each';
+import { eachArray, filterArray, mapArray } from '../array/each';
+import { eachObject, filterObject, mapObject } from '../object/each';
+import { hasValue, isArray, isFunction, isPlainObject } from '../internal/is';
 const forEachWrap = (object, funct) => {
   return object.forEach(funct);
 };
-const generateCheckLoops = (first, second) => {
+const generateCheckLoops = (arrayLoop, objectLoop) => {
   return (object, funct) => {
     let returned;
     if (!hasValue(object)) {
       return;
     } else if (isArray(object)) {
-      returned = first;
+      returned = arrayLoop;
     } else if (isPlainObject(object) || isFunction(object)) {
-      returned = second;
+      returned = objectLoop;
     } else if (object.forEach) {
       returned = forEachWrap;
     } else {
-      returned = second;
+      returned = objectLoop;
     }
     return returned(object, funct);
   };
@@ -27,7 +27,7 @@ export const map = generateCheckLoops(mapArray, mapObject);
 export const each = generateCheckLoops(eachArray, eachObject);
 export const filter = generateCheckLoops(filterArray, filterObject);
 assign(acid, {
-  map,
   each,
-  filter
+  filter,
+  map
 });
