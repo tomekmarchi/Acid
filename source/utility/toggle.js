9f2a7f8e1d32442f0a8db8ce125ced58d52acb82
Tomek Marchi <tomekmarchi@gmail.com>
2017-06-30 19:15:09 -0400

    Toggle patched

--- source/utility/toggle.js
+++ source/utility/toggle.js
@@ -1,6 +1,6 @@
-import { assign } from '../internal/object';
 import acid from '../namespace/index';
- /**
+import { assign } from '../internal/object';
+/**
  toggle does a strict comparison between the value and an argument. If it returns true, then it returns the b argument. Else it returns the a argument.
  @property  {value} - Can be any data type
  @property {on} -  Can be any data type. Is compared against value.
@@ -10,7 +10,8 @@ import acid from '../namespace/index';
  toggle(value, on)
  */
 export const toggle = (value, on, off) => {
- return (value === on) ? off : on;
-  };
- assign(acid, {
-   toggle
+  return (value === on) ? off : on;
+};
+assign(acid, {
+  toggle
+});
