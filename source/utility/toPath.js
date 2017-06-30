caafc2f798fda4320d23039fe9089cb1dc704c7c
Erick Delgado <taken404@gmail.com>
2017-06-30 16:21:03 -0400

    Utility Documentation
    Documentation of remaining functions

--- source/utility/toPath.js
+++ source/utility/toPath.js
@@ -3,7 +3,10 @@ import { assign } from '../internal/object';
 const regexToPath = /\.|\[/;
 const regexCloseBracket = /]/g;
 const emptyString = '';
-export const toPath = (string = emptyString) => {
+/**
+toPath replaces a closed bracket with an empty string and splits on an opening bracket and periods.
+*/
+export const toPath = (string) => {
   return string.replace(regexCloseBracket, emptyString).split(regexToPath);
 };
 assign(acid, {
--- source/utility/uuid.js
+++ source/utility/uuid.js
@@ -4,6 +4,9 @@ import { hasValue } from '../internal/is';
 let count = 0;
 const uuidFree = [];
 const uuidClosed = {};
+/**
+uuid returns a unique id
+*/
 export const uuid = () => {
   let result = uuidFree.shift(uuidFree);
   if (!hasValue(result)) {
