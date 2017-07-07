1acd9a059bb2c7b049effb905356facd994cf95f
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-07 14:53:02 -0400

    Object/Math Docs

--- source/number/math.js
+++ source/number/math.js
@@ -3,15 +3,67 @@ import { assign } from '../internal/object';
 const mathNative = Math;
 const floorMethod = mathNative.floor;
 const randomMethod = mathNative.random;
+/**
+  * Adds two numbers
+  *
+  * @function add
+  * @type {Function}
+  * @param {number} number - First number.
+  * @param {number} value - Second number.
+  * @returns {number} - Returns the sum of the arguments.
+  *
+  * @example
+  * add(1, 1);
+  * // => 2
+*/
 export const add = (number, value) => {
   return number + value;
 };
+/**
+  * Subtracts two numbers.
+  *
+  * @function minus
+  * @type {Function}
+  * @param {number} number - First number.
+  * @param {number} value - Second number.
+  * @returns {number} - Returns the difference of the arguments.
+  *
+  * @example
+  * add(1, 1);
+  * // => 0
+*/
 export const minus = (number, value) => {
   return number - value;
 };
+/**
+  * Divides two numbers.
+  *
+  * @function divides
+  * @type {Function}
+  * @param {number} number - First number.
+  * @param {number} value - Second number.
+  * @returns {number} - Returns the quotient of the arguments.
+  *
+  * @example
+  * add(10, 5);
+  * // => 2
+*/
 export const divide = (number, value) => {
   return number / value;
 };
+/**
+  * Multiplies two numbers.
+  *
+  * @function multiply
+  * @type {Function}
+  * @param {number} number - First number.
+  * @param {number} value - Second number.
+  * @returns {number} - Returns the product of the arguments.
+  *
+  * @example
+  * add(10, 5);
+  * // => 50
+*/
 export const multiply = (number, value) => {
   return number * value;
 };
--- source/object/assignDeep.js
+++ source/object/assignDeep.js
@@ -2,6 +2,21 @@ import acid from '../namespace/index';
 import { assign } from '../internal/object';
 import { eachObject } from './each';
 import { isArray, isPlainObject } from '../internal/is';
+/**
+  * Creates new object with deeply assigned values from another object/array.
+  *
+  * @function assignDeep
+  * @type {Function}
+  * @param {Object} object - Object to be assigned new properties.
+  * @param {Object} otherObject - Object from which properties are extracted.
+  * @param {Array} mergeArrays - Array from which items are assigned to the new object.
+  * @returns {Object} - Returns object with new properties.
+  *
+  * @example
+  * assignDeep({a:1}, {b:2})
+  * //=> {a:1, b:2}
+  *
+*/
 export const assignDeep = (object, otherObject, mergeArrays) => {
   eachObject(otherObject, (item, key) => {
     if (isPlainObject(item) && isPlainObject(object[key])) {
