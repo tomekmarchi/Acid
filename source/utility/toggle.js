c70f946cab2a817bb107afc91d7e889d4f2a3fef
Tomek Marchi <tomekmarchi@users.noreply.github.com>
2017-07-07 14:49:12 -0400

    Update toggle.js

--- source/utility/toggle.js
+++ source/utility/toggle.js
@@ -1,23 +1,63 @@
 import acid from '../namespace/index';
 import { assign } from '../internal/object';
-import { isEqual } from './isEqual';
+import { hasValue } from '../internal/is';
+let count = 0;
+const uuidFree = [];
+const uuidClosed = {};
 /**
-  * Performs a toggle between 2 values using a deep or strict comparison.
+  * Creates a numerical unique ID and recycles old ones. UID numerically ascends however freed UIDs are later reused.
   *
-  * @function toggle
+  * @function uid
   * @type {Function}
-  * @param  {(string|number|Object|Array)} value - Strictly compared against the on argument.
-  * @param {(string|number|Object|Array)} on -  Strictly compared against the value argument.
-  * @param {(string|number|Object|Array)} off -  Value to be returned.
-  * @returns {(string|number|Object|Array)} - The opposing value to the current.
+  * @returns {number} - Returns a unique id.
   *
   * @example
-  * toggle(1, 2, 3);
-  * //=> 2
+  * uid();
+  * //=> 0
+  *
+  * uid();
+  * //=> 1
+  *
+  * uid.free(0);
+  * //=> undefined
+  *
+  * uid();
+  * //=> 0
 */
-export const toggle = (value, on, off) => {
-  return (isEqual(on, value)) ? off : on;
+export const uid = () => {
+  let result = uuidFree.shift(uuidFree);
+  if (!hasValue(result)) {
+    result = count;
+    uuidClosed[result] = true;
+    count++;
+  }
+  return result;
+};
+/**
+  * Frees an UID so that it may be recycled for later use.
+  *
+  * @function uid
+  * @type {Function}
+  * @param {number} uid - Number to freed.
+  * @returns {undefined} - Nothing is returned.
+  *
+  * @example
+  * uid();
+  * //=> 0
+  *
+  * uid();
+  * //=> 1
+  *
+  * uid.free(0);
+  * //=> undefined
+  *
+  * uid();
+  * //=> 0
+*/
+uid.free = (id) => {
+  uuidClosed[id] = null;
+  uuidFree.push(id);
 };
 assign(acid, {
-  toggle
+  uid,
 });
