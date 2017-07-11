eb2e0584c29729aacff69beaa2f43b2fbbb962a0
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-10 22:05:39 -0400

    Collection naming edits/ Array Docs

--- source/array/clear.js
+++ source/array/clear.js
@@ -1,5 +1,17 @@
 import acid from '../namespace/index';
 import { assign } from '../internal/object';
+/**
+  * Clears the values out of an array.
+  *
+  * @function clear
+  * @type {Function}
+  * @param {Array} array - Takes an array to be emptied.
+  * @returns {Array} The originally given array.
+  *
+  * @example
+  * clear([1,'B', Cat]);
+  * //=> []
+*/
 export const clear = (array) => {
   array.length = 0;
   return array;
