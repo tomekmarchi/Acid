5c079ce61d6d1e292c9ba4b4f7917f212cc1efb6
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-11 03:45:19 -0400

    Array Doc 2

--- source/array/object.js
+++ source/array/object.js
@@ -1,7 +1,19 @@
 import acid from '../namespace/index';
 import { assign } from '../internal/object';
 import { eachArray } from './each';
-// Converts arrays into objects.
+/**
+  * Takes all but the last item in the array.
+  *
+  * @function arrayToObject
+  * @type {Function}
+  * @param {Array} array - Array to have items extracted from.
+  * @param {*} properties - Array to have items extracted from.
+  * @returns {Array} - Returns a completely flattened array.
+  *
+  * @example
+  * initial( [1, 2, 3, 4, 5]);
+  * // => [1, 2, 3, 4]
+*/
 export const arrayToObject = (values, properties) => {
   const sortedObject = {};
   eachArray(values, (item, key) => {
--- source/array/partition.js
+++ source/array/partition.js
@@ -2,6 +2,24 @@ import acid from '../namespace/index';
 import { assign } from '../internal/object';
 import { compactMapArray } from './each';
 // Split array into two arrays: one whose elements all satisfy predicate and one whose elements all do not satisfy predicate.
+/**
+  * Split array into two arrays: one whose elements all satisfy predicate and one whose elements all do not satisfy predicate.
+  *
+  * @function partition
+  * @type {Function}
+  * @param {Array} array - Takes an array to split.
+  * @param {Function} funct - Function run on each item in array.
+  * @returns {Array} - One array split into two arrays.
+  *
+  * @example
+  *const users = [
+  *{ 'user': 'barney',  'age': 36, 'active': false },
+  *{ 'user': 'fred',    'age': 40, 'active': true },
+  *{ 'user': 'pebbles', 'age': 1,  'active': false }
+  *];
+  * partition(users, function(o) { return o.active; });
+  *  // => objects for [['fred'], ['barney', 'pebbles']]
+*/
 export const partition = (array, funct) => {
   const failed = [];
   return [
