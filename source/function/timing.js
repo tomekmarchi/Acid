da2afa1ae939046ccc8a471108eb314adc26357a
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-17 00:28:49 -0400

    Timing Function Docs

--- source/function/timing.js
+++ source/function/timing.js
@@ -1,9 +1,35 @@
 import acid from '../namespace/index';
 import { assign } from '../internal/object';
 import { times } from '../array/times';
+/**
+  * A wrapper around setTimeout.
+  *
+  * @function timer
+  * @param {Object} method - Function to be invoked.
+  * @param {number} time - The time in nanoseconds.
+  * @type {Function}
+  * @returns {number} Set timeout id.
+  *
+  * @example
+  * timer(() => { return 1}, 1);
+  * // => 0: 1
+*/
 export const timer = (method, time) => {
   return setTimeout(method, time);
 };
+/**
+  * Wrapper around setInterval.
+  *
+  * @function interval
+  * @type {Function}
+  * @param {Object} method - Function to be invoked.
+  * @param {number} time - The time in nanoseconds.
+  * @returns {Object} Returns the new empty object.
+  *
+  * @example
+  * interval(() => { return 1}, 1);
+  * // => 0: 1
+*/
 export const interval = (method, time) => {
   return setInterval(method, time);
 };
@@ -14,8 +40,47 @@ const generateClear = (method, clearMethod) => {
     });
   };
 };
+/**
+  * Clears setTimeout function.
+  *
+  * @function clearTimers
+  * @type {Function}
+  * @param {Object} timer - Timer to be cleared.
+  * @param {Function} clearTimeout - Invocation of time clear function.
+  * @returns {Number} Returns a number of the cleared setTimeout ID.
+  *
+  * @example
+  *
+  * // =>
+*/
 export const clearTimers = generateClear(timer, clearTimeout);
+/**
+  * Clears setInterval function.
+  *
+  * @function clearIntervals
+  * @type {Function}
+  * @param {Object} interval - Interval to be cleared.
+  * @param {Object} clearInterval - Invocation of interval clear function.
+  * @returns {Number} Returns a number of the cleared setInterval ID.
+  *
+  * @example
+  * clearIntervals();
+  * // =>
+*/
 export const clearIntervals = generateClear(interval, clearInterval);
+/**
+  *
+  *
+  * @function debounce
+  * @type {Function}
+  * @param {Object} original -
+  * @param {Object} time -
+  * @returns {Object}
+  *
+  * @example
+  * debounce();
+  * // =>
+*/
 export const debounce = (original, time) => {
   let timeout = false;
   const debounced = (...args) => {
@@ -35,6 +100,19 @@ export const debounce = (original, time) => {
   };
   return debounced;
 };
+/**
+  *
+  *
+  * @function throttle
+  * @type {Function}
+  * @param {Object} method -
+  * @param {Object} time -
+  * @returns {Object} Returns the new empty object.
+  *
+  * @example
+  * throttle();
+  * // =>
+*/
 export const throttle = (method, time) => {
   let timeout = false;
   let shouldThrottle;
