fd95ed3fd261f8e7b0be604235c9b0b39083c51f
Tomek Marchi <tomekmarchi@gmail.com>
2017-07-19 13:46:39 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	source/browser/ready.js

--- source/browser/is.js
+++ source/browser/is.js
@@ -4,3 +4,25 @@ import { isSameObjectGenerator, objectStringGenerate } from '../internal/is';
 eachArray(['HTMLCollection', 'NodeList'], (item) => {
   acid[`is${item}`] = isSameObjectGenerator(objectStringGenerate(item));
 });
+/**
+ * Checks if the value is a HTMLCollection.
+ *
+ * @function isHTMLCollection
+ * @param {*} value - Object to be checked.
+ * @returns {boolean} True or false.
+ *
+ * @example
+ * isHTMLCollection(document.getElementsByClassName('test'));
+ * // => true
+*/
+/**
+ * Checks if the value is a NodeList.
+ *
+ * @function isNodeList
+ * @param {*} value - Object to be checked.
+ * @returns {boolean} True or false.
+ *
+ * @example
+ * isNodeList(document.querySelectorAll('.test'));
+ * // => true
+*/
