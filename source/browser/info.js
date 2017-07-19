2ae60d3f7c28e8d82b9b9dc6b066db7cb6ff3d4f
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-19 14:30:26 -0400

    Browser doc patch

--- source/browser/info.js
+++ source/browser/info.js
@@ -4,7 +4,7 @@ const protocol = location.protocol;
 const protocolSocket = (protocol === 'http:') ? 'ws' : 'wss';
 const hostname = location.hostname;
 /**
-  * Holds client hardware and host info.
+  * Holds client hardware, browser, and host info.
   *
   * @memberof $
   * @property info
