f9f7e9e176f55e4447a25fcb3b074e12066d2b93
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-19 14:24:21 -0400

    Event Docs

--- source/browser/event.js
+++ source/browser/event.js
@@ -1,9 +1,39 @@
 import acid from '../namespace/index';
 import { assign } from '../internal/object';
+/**
+  * Attaches an event listener to a node.
+  *
+  * @function eventAdd
+  * @type {Function}
+  * @param {Node} node - Given node.
+  * @param {string} type - A string representing the event type.
+  * @param {Object|Function} listener - The object which receives a notification when an event of the specified type occurs.
+  * @param {Object} options - An options object that specifies characteristics about the event listener.
+  * @returns {Node} - Returns given node.
+  *
+  * @example
+  * eventAdd(document.body, 'click', () => {console.log('CLICKED');});
+  * // = > document.body
+*/
 export const eventAdd = (node, ...args) => {
   node.addEventListener(...args);
   return node;
 };
+/**
+  * Attaches an event listener to a node.
+  *
+  * @function eventRemove
+  * @type {Function}
+  * @param {Node} node - Given node.
+  * @param {string} type - A string representing the event type.
+  * @param {Object|Function} listener - An object|function representing the listener.
+  * @param {Object} options - An options object that specifies characteristics about the event listener.
+  * @returns {undefined} - Undefined.
+  *
+  * @example
+  * eventRemove(document.body, () => {console.log('CLICKED');});
+  * // = > Undefined
+*/
 export const eventRemove = (node, ...args) => {
   node.removeEventListener(...args);
   return node;
