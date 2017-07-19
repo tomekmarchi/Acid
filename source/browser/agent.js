0f5ad16a21f73867c7b9eea0d1ca0f1e183de399
Tomek Marchi <tomekmarchi@gmail.com>
2017-07-19 12:28:08 -0400

    Removed Files + Browser docs
    Removed jsDocPlugins, browser/appstate
    Removed exports for attribute, fragment

--- source/browser/agent.js
+++ source/browser/agent.js
@@ -1,8 +1,20 @@
 import acid from '../namespace/index';
 import { eachArray } from '../array/each';
 import { assign, keys } from '../internal/object';
-const isAgent = (string) => {
-  return (string) ? isAgent[string] : keys(isAgent);
+/**
+  * Checks to see of the browser agent has a string.
+  *
+  * @function isAgent
+  * @type {Function}
+  * @param {string} value - The string to search for.
+  * @returns {boolean} Returns true or false.
+  *
+  * @example
+  * isAgent('mobile');
+  * // => false
+*/
+const isAgent = (value) => {
+  return (value) ? isAgent[value] : keys(isAgent);
 };
 let userAgentNormalized = navigator.userAgent.toLowerCase();
 userAgentNormalized = userAgentNormalized.replace(/_/g, '.');
--- source/browser/appState.js
+++
@@ -1,6 +0,0 @@
-import acid from '../namespace/index';
-import { assign } from '../internal/object';
-export const appState = {};
-assign(acid, {
-  appState
-});
