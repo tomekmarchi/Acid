a905b4171975da8341d4cc15a961ef3ca1ae7ae0
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-19 13:24:54 -0400

    Ready docs

--- source/browser/ready.js
+++ source/browser/ready.js
@@ -2,14 +2,26 @@ import acid from '../namespace/index';
 import { assign } from '../internal/object';
 import { eventAdd } from './event';
 import { importjs } from './import';
-export const isDocumentReady = (func) => {
+/**
+  * Runs a function if the document has finished loading.
+  *
+  * @function  isDocumentReady
+  * @type {Function}
+  * @param {Function} callable - Object to be checked.
+  * @returns {boolean} Returns true if the keycode property of the object equals thirteen and vice versa.
+  *
+  * @example
+  * isEnter('click')
+  * // => false
+*/
+export const isDocumentReady = (callable) => {
   const state = document.readyState;
   const checkStatus = state === 'interactive' || state === 'completed' || state === 'complete';
   if (checkStatus) {
-    return (func) ? func() : true;
+    return (callable) ? callable() : true;
   }
-  if (func) {
-    eventAdd(document, 'DOMContentLoaded', func);
+  if (callable) {
+    eventAdd(document, 'DOMContentLoaded', callable);
   }
   return false;
 };
